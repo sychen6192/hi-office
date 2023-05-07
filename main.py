aeb115dd410add4bb3acabf783faf43edd5b7d94
Jack SY Chen <jack_sy_chen@trendmicro.com>
2023-05-07 19:16:59 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import subprocess
+
+commands = f'''
+@echo off
+(cd /d "%~dp0")&&(NET FILE||(powershell start-process -FilePath '%0' -verb runas)&&(exit /B)) >NUL 2>&1
+title Office 2019 Activator r/Piracy
+echo Converting... & mode 40,25
+(if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office16")&(if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16")&(for /f %%x in ('dir /b ..\root\Licenses16\ProPlus2019VL*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)&(for /f %%x in ('dir /b ..\root\Licenses16\ProPlus2019VL*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)
+cscript //nologo ospp.vbs /unpkey:6MWKP >nul&cscript //nologo ospp.vbs /inpkey:NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP >nul&set i=1
+:server
+if %i%==1 set KMS_Sev=kms7.MSGuides.com
+if %i%==2 set KMS_Sev=kms8.MSGuides.com
+if %i%==3 set KMS_Sev=kms9.MSGuides.com
+cscript //nologo ospp.vbs /sethst:%KMS_Sev% >nul
+echo %KMS_Sev% & echo Activating...
+cscript //nologo ospp.vbs /act | find /i "successful" && (echo Complete) || (echo Trying another KMS Server & set /a i+=1 & goto server)
+pause >nul
+exit
+'''
+def run_cmd(cmd):
+    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+    return completed
+
+def read_cmd(cmds):
+    a = cmds.split('\n')
+    return a
+
+if __name__ == '__main__':
+    cmds = read_cmd(commands)
+    for cmd in cmds:
+        try:
+            if cmd:
+                res = run_cmd(cmd)
+                if res.returncode != 0:
+                    raise Exception(f"An error occured: {res.stderr}")
+        except Exception as err:
+            print(err)
